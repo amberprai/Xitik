3369066cebcc4e9ff8c57fef1fb0ca40634d9740
Amber Prakash Rai <ambarrai7@gmail.com>
2020-06-14 12:37:57 +0530

    initial commit

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PageNotFound = () =>{
     return (
-        <div>
-            <h1>NotFound Page</h1>
+        <div className="pt-5 pb-5">
+            <h1 className="pt-5 pb-5">NotFound Page</h1>
         </div>
     )
 }
