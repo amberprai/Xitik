4a1c0dc5ddc1c080090c01c01a5726e44be2c38d
Amber Prakash Rai <ambarrai7@gmail.com>
2020-06-28 00:01:05 +0530

    initial commit

--- src/Components/NewProductPage.js
+++ src/Components/NewProductPage.js
@@ -20,84 +20,84 @@ const NewProductPage = () => {
               
                 <li class="list-group-item">
                 
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                            <a href="product1"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}} >LED BULB</h5></a>
-                        </div><img src={led} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={led} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2 "/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
                 
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product2"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>INVERTER LED BULB</h5></a>
-                        </div><img src={led} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={led} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
 
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product3"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>TOUCH WIFI ON/OFF + WI-FI FAN + DIMMER</h5></a>
-                        </div><img src={pic1} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic1} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
         
                 <li class="list-group-item">
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product4"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>TOUCH WIFI ON/OFF 8G</h5></a>
                         </div>
-                        <img src={pic2} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        <img src={pic2} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
             
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product5"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>TOUCH WIFI ON/OFF SOCKET+SWITCH</h5></a>
-                        </div><img src={pic3} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic3} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product6"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>WI-FI ON/OFF MCB</h5></a>
-                        </div><img src={pic7} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic7} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product7"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>WI-FI ON/OFF SWITCH MODULE+CONTROLLER</h5></a>
-                        </div><img src={pic8} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic8} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product8"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>WI-FI ON/OFF SWITCH MODULE</h5></a>
-                        </div><img src={pic9} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic9} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product9"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>DIAMOND SHAPE TOUCH WI-FI ON/OFF SWITCH</h5></a>
-                        </div><img src={pic10} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic10} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
                 <li class="list-group-item">
             
-                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-2">
                         <div class="media-body order-2 order-lg-1">
                           <a href="product10"> <h5 class="mt-0 font-weight-bold mb-2" style={{color:"black"}}>REMOTE CONTROL SWITCH</h5></a>
-                        </div><img src={pic11} alt="Generic placeholder image" width="250" class="ml-lg-5 order-1 order-lg-2"/>
+                        </div><img src={pic11} alt="Generic placeholder image" width="240" class="ml-lg-5 order-1 order-lg-2"/>
                     </div> 
                 </li> 
             </ul> 
